31d04ccb3260234a288bda9fb1187148da823bc0
KarimovaFatima <karimovafatima127@gmail.com>
2024-08-29 14:23:03 +0400

    initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+// ! exercise 1.1
+// function getFullName(name, surname) {
+//     console.log(name, " ", surname)
+// }
+// getFullName("Lamiyə", "Kazımova");
+
+// ! exercise 1.2
+// function getFullName(country, city, street) {
+//     console.log("ölkə =>", country, ",", "şəhər => ", city, ",", "küçə =>", street)
+// }
+// getFullName("Azərbaycan", "Bakı", "Nizami");
+
+// ! exercise 1.3
+// function graduates(grade) {
+//     if (grade>=90) {
+//         console.log("əla");
+//     } else if(grade>=50){
+//         console.log("orta");
+//     }
+//     else{
+//         console.log("kəsr")
+//     }
+// }
+// graduates(95);
+
+// ! exercise 2.3
+// function kmToMile(a) {
+//     let mile = a * 0.621371;
+//     console.log(mile);
+// }
+// kmToMile(5);
+
+// ! exercise 2.4
+// let arr = [23,25,129,1,8];
+// let sumPrice = arr.reduce((counter, sum) => {
+//   return counter + sum;
+// }, 0);
+
+// ! exercise 2.
+// function isDivisibleBy10(number) {
+//     return number % 10 === 0;
+// }
+// console.log(isDivisibleBy10(50));
+
+
+// ! exercise 2.
+
+// function findVowels(str) {
+//     let vowels = ['a','ı','o','u','e','ə','i','ö','ü'];
+//     let foundVowels = [];
+//     for (let i = 0; i < str.length; i++) {
+//         if (vowels.includes(str[i])) {
+//             foundVowels.push(str[i]);
+//         }
+//     }
+//     return foundVowels;
+// }
+// console.log(findVowels("Hello World"));
+
+
